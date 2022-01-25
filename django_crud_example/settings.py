7cd3787b6c6dd91f7c4955ffa3ad57ac785501a0
resolt <pdmmichaelsen@gmail.com>
2022-01-25 21:07:02 +0100

    added shell_plus

--- django_crud_example/settings.py
+++ django_crud_example/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clientapi'
+    'django_extensions',
+    'clientapi',
 ]
 
 MIDDLEWARE = [
