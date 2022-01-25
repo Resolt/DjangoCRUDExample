7ed875fdd726b5a92cd9683b77fb0bb662c3da07
resolt <pdmmichaelsen@gmail.com>
2022-01-25 17:39:01 +0100

    more poetry, gitignore and first example endpoint

--- django_crud_example/urls.py
+++ django_crud_example/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 
+
 api = NinjaAPI()
 
 @api.get("/add")
-def add(requests, a: int, b: int):
+def add(request, a: int, b: int):
     return {"result": a + b}
 
 urlpatterns = [
